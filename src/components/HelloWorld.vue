8dc9521b565ddf5f6c26de44f7183b2b2a3db240
houxiaoming <601277372@qq.com>
2019-05-05 17:24:16 +0800

    什么情况

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <p style="display: inline">00小时{{minute}}分{{second}}秒</p>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {
+        goodsObj: [
+          {
+            name: 'HomeMinuteTimer',
+            checked: false,
+            list: [ ]
+          }
+        ],
+        minutes: 59,   // 此处修改分钟数量
+        seconds: 59     // 此处修改秒数数量
+      }
+    },
+    mounted () {
+      this.add()
+    },
+    methods: {
+      num: function (n) {
+        return n < 10 ? '0' + n : '' + n
+      },
+      add: function () {
+        var _this = this
+        var time = window.setInterval(function () {
+          if (_this.seconds === 0 && _this.minutes !== 0) {
+            _this.seconds = 59
+            _this.minutes -= 1
+          } else if (_this.minutes === 0 && _this.seconds === 0) {
+            _this.seconds = 0
+            window.clearInterval(time)
+          } else {
+            _this.seconds -= 1
+          }
+        }, 1000)
+      }
+    },
+    watch: {
+      second: {
+        handler (newVal) {
+          this.num(newVal)
+        }
+      },
+      minute: {
+        handler (newVal) {
+          this.num(newVal)
+        }
+      }
+    },
+    computed: {
+      second: function () {
+        return this.num(this.seconds)
+      },
+      minute: function () {
+        return this.num(this.minutes)
+      }
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
